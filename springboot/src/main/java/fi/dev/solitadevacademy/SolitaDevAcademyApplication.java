71169433705450ad69b0faa0ac57cf79c08b683b
seveneleven337 <solidxnake_6@hotmail.com>
2023-04-26 00:23:38 +0300

    springboot initialized

---
+++ springboot/src/main/java/fi/dev/solitadevacademy/SolitaDevAcademyApplication.java
@@ -0,0 +1,13 @@
+package fi.dev.solitadevacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolitaDevAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolitaDevAcademyApplication.class, args);
+    }
+
+}
---
+++ springboot/src/test/java/fi/dev/solitadevacademy/SolitaDevAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.dev.solitadevacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolitaDevAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
